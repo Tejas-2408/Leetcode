2f48ae67c8c9fad82c4a48cbd25ba22396dcc710
Tejas Bansal <tejasbansal6065@gmail.com>
2026-01-12 03:23:59 +0530

    max consecutive ones

--- JavaSol/src/array/MaxConsecutiveOnes3.java
+++ JavaSol/src/array/MaxConsecutiveOnes3.java
@@ -4,6 +4,65 @@ package array;
 
 public class MaxConsecutiveOnes3 {
     public static void main(String[] args) {
+        int[] nums = {1,1,1,0,0,0,1,1,1,1,0};
+        int k = 2;
+        System.out.println(longestOnes(nums,k));
+        System.out.println(longestOnesOptimised(nums,k));
+    }
 
+    // O(n)
+    public static int longestOnesOptimised(int[] nums, int k){
+        int l = 0;
+        int r = 0;
+        int maxLen = 0;
+        int zeros = 0;
+
+        while(r < nums.length){
+            if(nums[r] == 0){
+                zeros++;
+            }
+
+            if(zeros > k){
+                if(nums[l] == 0){
+                    zeros--;
+                }
+                l++;
+            }
+
+            if(zeros <= k){
+                maxLen = Math.max(maxLen,r-l+1);
+            }
+
+            r++;
+        }
+        return maxLen;
+    }
+
+    // O(NlogN) complexity
+    public static int longestOnes(int[] nums, int k){
+        int l = 0;
+        int r = 0;
+        int maxLen = 0;
+        int zeros = 0;
+
+        while(r < nums.length){
+            if(nums[r] == 0){
+                zeros++;
+            }
+
+            while(zeros > k){
+                if(nums[l] == 0){
+                    zeros--;
+                }
+                l++;
+            }
+
+            if(zeros <= k){
+                maxLen = Math.max(maxLen,r-l+1);
+            }
+
+            r++;
+        }
+        return maxLen;
     }
 }
