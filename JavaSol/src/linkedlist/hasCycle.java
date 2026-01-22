3e0ef1da1fe31c1b11f04fdf949020824d36fe93
Tejas Bansal <tejasbansal6065@gmail.com>
2026-01-22 22:19:23 +0530

    hasCycle

---
+++ JavaSol/src/linkedlist/hasCycle.java
@@ -0,0 +1,14 @@
+package linkedlist;
+
+// https://leetcode.com/problems/linked-list-cycle/description/?envType=problem-list-v2&envId=linked-list
+
+public class hasCycle {
+    public static void main(String[] args) {
+
+        ListNode ln = new ListNode(4);
+        ln.hasCycle(ln);
+
+    }
+
+
+}
