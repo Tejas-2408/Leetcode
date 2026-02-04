6236ec4e3dd0d96be4a8d4bf036642fbae39c3a9
Tejas Bansal <tejasbansal6065@gmail.com>
2026-02-04 23:25:40 +0530

    stack using queue

--- JavaSol/src/stacksQueues/StackWithQueues.java
+++ JavaSol/src/stacksQueues/StackWithQueues.java
@@ -1,43 +1,25 @@
 package stacksQueues;
 
 import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.Queue;
 
 public class StackWithQueues {
-    private Queue<Integer> first;
-    private Queue<Integer> second;
+    private Deque<Integer> first;
 
     public StackWithQueues() {
         first = new ArrayDeque<>();
-        second = new ArrayDeque<>();
     }
-
     public void push(int x) {
         first.add(x);
     }
 
     public int pop() {
-        while (!first.isEmpty()){
-            second.add(first.remove());
-        }
-
-        int removed = second.remove();
-        while (!second.isEmpty()){
-            first.add(second.remove());
-        }
-        return removed;
+        return first.removeLast();
     }
 
     public int top() {
-        while (!first.isEmpty()){
-            second.add(first.remove());
-        }
-
-        int peeked = second.peek();
-        while (!second.isEmpty()){
-            first.add(second.remove());
-        }
-        return peeked;
+        return first.peekLast();
     }
 
     public boolean empty() {
