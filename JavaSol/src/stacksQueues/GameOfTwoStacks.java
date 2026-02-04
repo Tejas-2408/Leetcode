1e6b7d82d09e99e5004858c5c604c2b16d0dd896
Tejas Bansal <tejasbansal6065@gmail.com>
2026-02-04 23:44:35 +0530

    game of two stacks

---
+++ JavaSol/src/stacksQueues/GameOfTwoStacks.java
@@ -0,0 +1,9 @@
+package stacksQueues;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GameOfTwoStacks {
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+}
