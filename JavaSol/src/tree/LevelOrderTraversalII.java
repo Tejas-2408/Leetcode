7fc2abdac3a00bcb6ef918207314541feffb8531
Tejas Bansal <tejasbansal6065@gmail.com>
2026-06-06 04:28:39 +0530

    Level order traversal 2

---
+++ JavaSol/src/tree/LevelOrderTraversalII.java
@@ -0,0 +1,48 @@
+package tree;
+
+// https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/?envType=problem-list-v2&envId=tree
+
+import java.util.*;
+
+public class LevelOrderTraversalII {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        LinkedList<List<Integer>> result = new LinkedList<>();
+        if(root == null){
+            return result;
+        }
+        Queue<TreeNode> qu = new ArrayDeque<>();
+        qu.offer(root);
+
+        while(!qu.isEmpty()){
+            int levelSize = qu.size();
+            List<Integer> currentLevelList = new ArrayList<>();
+            for(int i = 0 ; i < levelSize ; i++){
+                TreeNode currentNode = qu.poll();
+                currentLevelList.add(currentNode.val);
+                if(currentNode.left != null){
+                    qu.offer(currentNode.left);
+                }
+
+                if(currentNode.right != null){
+                    qu.offer(currentNode.right);
+                }
+            }
+            result.addFirst(currentLevelList);
+        }
+        return result;
+
+    }
+}
