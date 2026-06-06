a41ed76feb03f54aaaad67283b18fcb208deb493
Tejas Bansal <tejasbansal6065@gmail.com>
2026-06-06 18:18:56 +0530

    Check symmetry

--- JavaSol/src/tree/CheckSymmetry.java
+++ JavaSol/src/tree/CheckSymmetry.java
@@ -2,6 +2,10 @@ package tree;
 
 // https://leetcode.com/problems/symmetric-tree/?envType=problem-list-v2&envId=tree
 
+import java.util.ArrayDeque;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class CheckSymmetry {
     public class TreeNode{
         int val;
@@ -20,5 +24,35 @@ public class CheckSymmetry {
         }
 
         return left.val == right.val && checkSymm(left.left,right.right) && checkSymm(left.right,right.left);
+    }
+
+    // BFS approach
+    public boolean isSymmetricc(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root.left);
+        queue.add(root.right);
+
+        while(!queue.isEmpty()){
+            TreeNode left = queue.poll();
+            TreeNode right = queue.poll();
+
+            if(left == null && right == null){
+                continue;
+            }
+
+            if(left == null || right == null){
+                return false;
+            }
+
+            if(left.val != right.val){
+                return false;
+            }
+
+            queue.add(left.left);
+            queue.add(right.right);
+            queue.add(left.right);
+            queue.add(right.left);
+        }
+        return true;
     }
 }
